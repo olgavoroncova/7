766e6ec217c5ddea7041dacb70acd1e52a478fa7
Olga <voron7zova.o@yandex.ru>
2022-11-10 22:41:18 +0300

    С#7ДЗ

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n,
+ заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-9, 10) * new Random().NextDouble();
+            Console.Write($"{matrix[i, j]:f2} \t");
+        }
+        Console.WriteLine();
+    }
+
+
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[n, m];
+InputMatrix(matrix);
+
+/*Задача 50. Напишите программу, которая на вход 
+принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1 7 -> числа с такими индексами в массиве нет
+*/
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 10, int rightRange = 99)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix = FillMatrix(4, 4);
+PrintMatrix(matrix);
+Console.WriteLine("Введите индекс строки");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс столбца");
+int n = Convert.ToInt32(Console.ReadLine());
+void FindIndexMatrix(int[,] matrix)
+{
+    if (m >= 0 && m < matrix.GetLength(0) && n >= 0 && n < matrix.GetLength(1))
+    {
+        Console.WriteLine(matrix[m, n]);
+    }
+    else
+    {
+        Console.WriteLine("Такого числа в массиве нет");
+    }
+}
+FindIndexMatrix(matrix);
+
